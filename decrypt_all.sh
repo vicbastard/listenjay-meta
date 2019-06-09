84a23ba72121664d8e2208c58853729b388e8dd3
Andrew Buchanan <buchanan@difference.com>
2019-06-09 16:24:40 -0400

    Add decrypt script

--- decrypt_all.sh
+++ decrypt_all.sh
@@ -2,5 +2,5 @@
 
 for VAULT in `find . | grep \.vault$`; do
   echo $VAULT
-  echo ansible-vault decrypt $VAULT --output `dirname $VAULT`/`basename $VAULT .vault` --vault-password-file=./decrypt.key
+  ansible-vault decrypt $VAULT --output `dirname $VAULT`/`basename $VAULT .vault` --vault-password-file=./decrypt.key
 done
